a295a07f11a2aeb795378ef7ebcd0b129a9fb39c
IceAgeQQ <d830300042@gmail.com>
2014-05-14 16:20:51 -0700

    eBay:iOS test

---
+++ eBay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eBay
+//
+//  Created by Chao Xu on 14-5-12.
+//  Copyright (c) 2014年 Chao Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ProximityKit/ProximityKit.h>
+#import <CoreLocation/CoreLocation.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate,PKManagerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) PKManager *proximityKitManager;
+
+@end
---
+++ eBay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  eBay
+//
+//  Created by Chao Xu on 14-5-12.
+//  Copyright (c) 2014年 Chao Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
